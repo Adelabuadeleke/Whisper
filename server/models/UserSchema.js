d71e8117be6e76d97e07453fc773e0ec93447be8
kamal <kamade123@gmail.com>
2023-01-22 17:05:23 +0100

    minor edit of user model schema

--- server/models/UserSchema.js
+++ server/models/UserSchema.js
@@ -10,4 +10,5 @@ const UserSchema = new mongoose.Schema({
 
 UserSchema.index({ email: 1 }, { unique: true });
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
